d08249442ebb65eed199b46802331d402bd12704
Jawad <rjava721@gmail.com>
2019-12-19 09:39:34 -0600

    wrap stops in a div, add direction and cross-street info

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let mainButton = document.querySelector('#main-button');
 let stopsElement;
 let streetKey;
 let streetStopsKeys = [];
-let streetStopsNames = [];
 let count = 0;//
 // we need to find a street
 // and then display the next 2 upcoming bus times in all the stops of the street
@@ -16,29 +15,33 @@ mainButton.addEventListener('click', function (event) {
 
     document.body.insertAdjacentHTML('beforeend',
     `<div id=street-stops>
-      <h2>Street-key: ${streetKey} -- Street-Name: ${streetData.streets[0].name}</h2>
+      <h3>Street-key: ${streetKey} -- Street-Name: ${streetData.streets[0].name}</h3>
     </div>`);
     stopsElement = document.querySelector('#street-stops');
 
     fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=jJp4zgyfpCcZGyAYLyM`)
     .then((stopsFetch) => stopsFetch.json())
     .then((stopsData) => {
+      console.log(stopsData);
+
       stopsData.stops.forEach((element,index) => {
         streetStopsKeys[index] = element.key;
-        streetStopsNames[index] = element.name;
       })
 
       streetStopsKeys.forEach((element, index) => {
         stopsElement.insertAdjacentHTML('beforeend', `
-        <p>Stop Name : ${streetStopsNames[index]} -- Stop Number : ${element}`);
+        <div id='street-stops' data-street-stop='${stopsData.stops[index].name}'>
+        <h4>Stop Name : ${stopsData.stops[index].name} -- Stop Number : ${element}</h4>
+        <h4>Direction : ${stopsData.stops[index].direction}</h4>
+        <h4>Cross-street: ${stopsData.stops[index]["cross-street"].name}</h4>
+        <p>===============================================</p>
+        </div`);
 
         fetch(`https://api.winnipegtransit.com/v3/stops/${element}/schedule.json?max-results-per-route=2&api-key=jJp4zgyfpCcZGyAYLyM`)
         .then(stopKeysFetch => stopKeysFetch.json()) 
-        .then((parsedStops) =>  {
-          stopsElement.insertAdjacentHTML('afterbegin', `
-          <p>direction : ${parsedStops["stop-schedule"].stop.direction}`);
-          console.log(parsedStops["stop-schedule"]);
-          console.log(`above was parsedStops`);// we want the names, route schedules[index].route / .name
+        .then((parsedStopsKeys) =>  {
+          console.log(parsedStopsKeys);
+
         })
       })
     })
