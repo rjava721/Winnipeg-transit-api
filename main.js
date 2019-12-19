afe067f86d67f1350e862684e828f73ec9586a62
Jawad <rjava721@gmail.com>
2019-12-19 11:53:52 -0600

    add estimated arrival time in each div

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+let street = 'Warde'
+let streetType = "avenue";
+
 let mainButton = document.querySelector('#main-button');
 let stopsElement;
+let eachStop;
+
 let streetKey;
 let streetStopsKeys = [];
-let count = 0;//
+
 // we need to find a street
 // and then display the next 2 upcoming bus times in all the stops of the street
 // /v3/stops.json?street=1902
 // /v3/streets.json?name=jubilee&type=avenue
 mainButton.addEventListener('click', function (event) {
-  fetch('https://api.winnipegtransit.com/v3/streets.json?name=henlow&type=bay&api-key=jJp4zgyfpCcZGyAYLyM')
+  
+  //first we get the street key
+  fetch(`https://api.winnipegtransit.com/v3/streets.json?name=${street}&type=${streetType}&api-key=jJp4zgyfpCcZGyAYLyM`)
   .then((streetFetch) => streetFetch.json())
   .then((streetData) => {
     streetKey = streetData.streets[0].key;
@@ -18,37 +25,51 @@ mainButton.addEventListener('click', function (event) {
       <h3>Street-key: ${streetKey} -- Street-Name: ${streetData.streets[0].name}</h3>
     </div>`);
     stopsElement = document.querySelector('#street-stops');
-
+    
+    //now we need to get the keys of each stops
     fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=jJp4zgyfpCcZGyAYLyM`)
     .then((stopsFetch) => stopsFetch.json())
     .then((stopsData) => {
-      console.log(stopsData);
+      //console.log(stopsData);
 
       stopsData.stops.forEach((element,index) => {
         streetStopsKeys[index] = element.key;
       })
 
+      console.log("streetStopsKeys.length :" + streetStopsKeys.length);
+
       streetStopsKeys.forEach((element, index) => {
         stopsElement.insertAdjacentHTML('beforeend', `
-        <div id='street-stops' data-street-stop='${stopsData.stops[index].name}'>
+        <div class='street-stop' data-street-stop='${stopsData.stops[index].name}'>
         <h4>Stop Name : ${stopsData.stops[index].name} -- Stop Number : ${element}</h4>
         <h4>Direction : ${stopsData.stops[index].direction}</h4>
         <h4>Cross-street: ${stopsData.stops[index]["cross-street"].name}</h4>
         <p>===============================================</p>
         </div`);
+        eachStop = document.getElementsByClassName('street-stop');
 
         fetch(`https://api.winnipegtransit.com/v3/stops/${element}/schedule.json?max-results-per-route=2&api-key=jJp4zgyfpCcZGyAYLyM`)
         .then(stopKeysFetch => stopKeysFetch.json()) 
         .then((parsedStopsKeys) =>  {
           console.log(parsedStopsKeys);
+          //console.log(parsedStopsKeys["stop-schedule"]["route-schedules"][index].route.number)
+          console.log('the length is ' + parsedStopsKeys["stop-schedule"]["route-schedules"].length);
 
+          parsedStopsKeys["stop-schedule"]["route-schedules"].forEach((element,index) => {
+
+            if(parsedStopsKeys["stop-schedule"]["route-schedules"].length !== 0) {// if there is no route schedule
+              console.log('lol' + parsedStopsKeys["stop-schedule"]["route-schedules"][index].route.number)
+              
+              for (let index = 0; index < eachStop.length; index++) {
+                eachStop[index].insertAdjacentHTML('beforeend', `
+                <h4>Estimated Arrival Time: </h4>`)
+              }
+            }
+
+          })
         })
       })
     })
   .catch((data) => console.log(`mess ${data}`));
   })
 });
-
-
-//consider wrapping all stops inside of a div with an id. 
-// select it and then you can specifically add to them the 2 next busses
